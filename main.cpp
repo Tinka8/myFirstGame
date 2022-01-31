e01f39b0e121943f875278cb5c5124e2945e57d0
Tinka8 <lenmatka818@gmail.com>
2022-01-31 15:15:25 +0100

    add choices

--- main.cpp
+++ main.cpp
@@ -42,12 +42,43 @@ int main() {
     cout << endl;
     // uvod
     cout << "Začínaš jako majitel zastávarny ve měste Bílina, v Ústeckém kraji." << endl;
-    cout << "Jmenuješ se " << name << " a je ti " << age << " let.";
+    cout << "Jmenuješ se " << name << " a je ti " << age << " let." << endl;
 
     cout << endl;
     // ciel hry
     cout << "Tvým úkolem v textové hře zastavarna je, aby ti neklesla reputace s žádným NPC pod 0" << endl;
-    cout << "Ve hře najdeš tato NPC: Kačka, Kačenka, Bibina, Africký šnek";
+    cout << "Ve hře najdeš tato NPC: Kačka, Kačenka, Bibina, Africký šnek" << endl;
+
+    cout << endl;
+    // vyber
+    cout << "Je pondělí ráno, budík zvoní v 7.30, vstáváš v 9.30 jako vždy." << endl;
+    cout << "Co uděláš nejdříve? " << endl;
+    cout << "1) Nakrmíš psa" << endl;
+    cout << "2) Připravíš si snídani" << endl;
+    cout << "3) Jdeš do koupelny" << endl;
+    cout << endl;
+    cout << "Vyber možnost: ";
+    cin >> choice;
+
+    // moznosti
+    if (choice == 1) {
+        cout << "Nakrmíš psa" << endl;
+    } else if (choice == 2) {
+        cout << "Připravíš si snídani";
+    } else if (choice == 3) {
+        cout << "Jdeš do koupelny";
+    }
+
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
